784d00471618db0233bb81d2cb7a15a9282d4ff4
lolso <rayovsky@gmail.com>
2023-05-30 09:53:17 +0400

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+//import { ref } from 'vue'
+import * as tf from '@tensorflow/tfjs';
+
+
+
+const loadModel = async () => {
+  return tf.loadGraphModel("/src/assets/Balls_model_js/model.json")
+}
+
+const start = async () => {
+  const model = await loadModel()
+  //model.execute()
+}
+
+const preprocess = async () => {
+  
+}
+
+</script>
+
+<template>
+  <img src="../assets/ball.png" width="500"/>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
