eef531e991d8a1bc084a5ecbd903b9ef57de7a79
lolso good <MY_NAME@example.com>
2023-05-30 15:37:59 +0400

    idk

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
-//import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import * as tf from '@tensorflow/tfjs';
+import * as fu from "./FileUpload"
 
-
+const file = ref<File | null>();
+const imageElement = ref<HTMLImageElement>();
 
 const loadModel = async () => {
   return tf.loadGraphModel("/src/assets/Balls_model_js/model.json")
@@ -10,17 +12,32 @@ const loadModel = async () => {
 
 const start = async () => {
   const model = await loadModel()
-  //model.execute()
+  let prediction = model.predict(preprocess())
+  console.log(Object.values(prediction))
 }
 
-const preprocess = async () => {
-  
+const preprocess = () => {
+  const imgTensor = tf.browser.fromPixels(imageElement.value!);
+  const resizedImg = tf.cast(imgTensor, 'float32')
+    .resizeBilinear([224, 224])
+    .div(tf.scalar(255.0));
+  const imgExpanded = tf.expandDims(resizedImg, 0);
+
+  return imgExpanded
 }
 
+/*function onFileChanged($event: Event) {
+            const target = $event.target as HTMLInputElement;
+            if (target && target.files) {
+                file.value = target.files[0];
+            }
+        }
+*/
 </script>
 
 <template>
-  <img src="../assets/ball.png" width="500"/>
+  <img ref="imageElement" src="../assets/Baseball.png" width="500"/>
+  <button @click="start">test</button>
 </template>
 
 <style scoped>
